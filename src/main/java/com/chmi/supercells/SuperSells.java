0475a06fdf376badea0055f8d3ee7ea7776ddf07
fsooaiety <dunnielbrits@gmail.com>
2026-02-26 19:22:42 +0700

    Fixed AE2 repository

---
+++ src/main/java/com/chmi/supercells/SuperSells.java
@@ -0,0 +1,11 @@
+package com.supersells;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("supersells")
+public class SuperSells {
+
+    public SuperSells() {
+        System.out.println("SuperSells loaded successfully!");
+    }
+}
